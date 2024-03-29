e2bc0fbcf8b6e5e2a2aca4ca1b020c55f03fdcc5
Narek Martirosyan <narekmartirosyanbm21@gmail.com>
2024-03-29 14:07:28 +0400

    last changes jQuery

--- js/script.js
+++ js/script.js
@@ -1,97 +1,104 @@
-function addToDisplay(value) {
-    let display = document.getElementById('display')
-    let lastCharIsOperator = display.value.length > 0 && ['/', '*', '-', '+'].includes(display.value.slice(-1));
-    let isOperator = ['/', '*', '-', '+'].includes(value);
-    if (lastCharIsOperator && isOperator) {
-        return;
-    }
-    display.value += value;
-}
+$(document).ready(function () {
+    let display = $('#display');
 
-document.querySelector('.calculator').addEventListener('click', function (event) {
-    if (event.target.classList.contains("number") || event.target.classList.contains("operator")){
-        addToDisplay(event.target.value)
-    } else if (event.target.classList.contains("equals")){
-        calculate()
-    } else if (event.target.classList.contains("clear")){
-        clearDisplay()
-    }
-})
+    $('.number').click(function () {
+        display.val(display.val() + $(this).val());
+    });
 
-function simulateBackspace() {
-    let inputElement = document.getElementById("display");
-    let currentValue = inputElement.value;
-    if (currentValue.length > 0) {
-        inputElement.value = currentValue.substring(0, currentValue.length - 1);
-    }
-}
-
-document.getElementById("display").addEventListener("keydown", function (event) {
-    if (event.key === "Backspace") {
-        event.preventDefault();
-        simulateBackspace();
-    }
-});
-
-function validateInput(input) {
-    input.value = input.value.replace(/[^0-9+\-*C/]/g, '');
-}
-
-function handleKeyPress(event) {
-    let display = document.getElementById('display');
-    let currentValue = display.value;
-    let lastChar = currentValue.slice(-1);
-    let keyPressed = String.fromCharCode(event.keyCode);
-
-    if (["+", "-", "*", "/"].includes(lastChar) && ["+", "-", "*", "/"].includes(keyPressed)) {
-        display.value = currentValue.slice(0, -1);
-    }
-}
-
-function clearDisplay() {
-    document.getElementById('display').value = '';
-}
-
-function calculate() {
-    let expression = document.getElementById('display').value;
-    if (expression) {
-        try {
-            document.getElementById('display').value = eval(expression);
-
-        } catch (error) {
-            alert("Please input valid data !!!")
-            document.getElementById('display').value = '';
+    $('.operator').click(function () {
+        let lastCharIsOperator = display.val().length > 0 && ['/', '*', '-', '+'].includes(display.val().slice(-1));
+        let isOperator = ['/', '*', '-', '+'].includes($(this).val());
+        if (lastCharIsOperator && isOperator) {
+            return;
         }
-    } else {
-        document.getElementById('display').value = '';
+        display.val(display.val() + $(this).val());
+    });
+
+    $('#clear').click(function () {
+        display.val('');
+    });
+
+
+    $('#calculate').click(function () {
+        if (display) {
+            try {
+                display.val(eval(display.val()));
+            } catch (error) {
+                alert("Please input valid data !!!")
+                display.val('')
+            }
+        } else {
+            display.val('')
+        }
+        if (isNaN(eval(display.val()))) {
+            alert("Not a valid data value !!!")
+            display.val('')
+        }
+        if (eval(display.val()) === Infinity) {
+            alert("Infinity value !!!")
+            display.val('')
+        }
+    });
+
+    function calculate() {
+        let expression = $('#display').val();
+        if (expression) {
+            try {
+                $('#display').val(eval(expression));
+            } catch (error) {
+                alert("Please input valid data !!!");
+                $('#display').val('');
+            }
+        } else {
+            $('#display').val('');
+        }
+
+        if (eval(expression) === Infinity) {
+            alert("Infinity value !!!");
+            $('#display').val('');
+        } else if (isNaN(eval(expression))) {
+            alert("Not a valid number value !!!");
+            $('#display').val('');
+        }
     }
 
-    if (eval(expression) === Infinity){
-        alert("Infinity value !!!")
-        document.getElementById('display').value = '';
+    $('#backspace').click(function () {
+        let displayVal = display.val()
+        display.val(displayVal.substring(0, displayVal.length - 1));
+    });
 
-    } else if (isNaN(eval(expression))){
-        alert("Not a valid number value !!!")
-        document.getElementById('display').value = '';
-    }
-}
+    $('#C').click(function () {
+        display.val('')
+    });
 
-document.getElementById("display").addEventListener("keydown", function (event) {
-    if (event.key === "=") {
-        event.preventDefault();
-     calculate()
-    }
-});
-document.getElementById("display").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        calculate()
-    }
-});
+    $("#display").on('input', function () {
+        $(this).val(function (i, val) {
+            return val.replace(/[^0-9+\-*.\/]/g, '');
+        });
+    });
 
-document.getElementById("display").addEventListener("keydown", function (event) {
-    if (event.key === "c") {
-        event.preventDefault();
-        clearDisplay()
+    function clearDisplay() {
+        $('#display').val('');
     }
-});
+
+    $('#display').on('keydown', function(event) {
+        if (event.key === "=" || event.key === "Enter") {
+            event.preventDefault();
+            calculate();
+        } else if (event.key === "c") {
+            event.preventDefault();
+            clearDisplay();
+        }
+    });
+
+    $('#display').on('keypress', function (event) {
+        let display = $(this);
+        let currentValue = display.val();
+        let lastChar = currentValue.slice(-1);
+        let keyPressed = String.fromCharCode(event.which);
+
+        if (["+", "-", "*", "/", "."].includes(lastChar) && ["+", "-", "*", "/", "."].includes(keyPressed)) {
+            display.val(currentValue.slice(0, -1));
+        }
+    })
+})
